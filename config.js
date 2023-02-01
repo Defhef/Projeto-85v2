2658ad278ed987a9d1ef4b8fffe84a9e215066b9
Defhef <105176513+Defhef@users.noreply.github.com>
2023-02-01 17:56:11 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCH9csXUaKdmhJLDsBpfgwWozs85xK1xL0",
+  authDomain: "projeto-85v2.firebaseapp.com",
+  databaseURL: "https://projeto-85v2-default-rtdb.firebaseio.com",
+  projectId: "projeto-85v2",
+  storageBucket: "projeto-85v2.appspot.com",
+  messagingSenderId: "987698331538",
+  appId: "1:987698331538:web:248f885043522fc5590b4b"
+};
